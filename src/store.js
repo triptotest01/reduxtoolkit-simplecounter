08ec2b7b003d12899c07ad6d6fadce6f7196f8d4
triptotest01 <triptotest01@gmail.com>
2023-04-22 13:51:18 +0900

    리덕스툴킷 원리 - 간단예제: 카운터

--- src/store.js
+++ src/store.js
@@ -3,13 +3,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 //방금 전 counter.js에서 export default한 counterSlice.reducer
-import counterSlice from "./redux/counter";
+import counterReducer from "./redux/counter";
 
 
 //redux store
 export const store = configureStore({
   reducer: {
     //add counterReducer
-    counter: counterSlice
+    counter: counterReducer
   },
 });
